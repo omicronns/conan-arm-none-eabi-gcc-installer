f5bce8a272f0921d2e086d4c7b77649d73abe37d
omicronns <omicronns@gmail.com>
2017-08-05 17:23:15 +0200

    added more versions, refactoring

--- conanfile.py
+++ conanfile.py
@@ -10,12 +10,15 @@ class ConanFileInst(conans.ConanFile):
     url = "https://github.com/omicronns/conan-arm-none-eabi-gcc-installer.git"
     settings = {"os": ["Windows", "Linux", "Macos"]}
 
-    year_quarter = {
-        "6.3": ("2017", "q2")
+    version_path_filename_map = {
+        "6.3.1-20170620": ("6-2017q2", "gcc-arm-none-eabi-6-2017-q2-update-%s"),
+        "6.3.1-20170215": ("6_1-2017q1", "gcc-arm-none-eabi-6-2017-q1-update-%s-zip"),
+        "6.2.1-20161205": ("6-2016q4", "gcc-arm-none-eabi-6_2-2016q4-20161216-%s-zip"),
+        "5.4.1-20160919": ("5_4-2016q3", "gcc-arm-none-eabi-5_4-2016q3-20160926-%s-zip"),
     }
 
-    options = {"version": list(year_quarter.keys())}
-    default_options = "version=6.3"
+    options = {"version": list(version_path_filename_map.keys())}
+    default_options = "version=6.3.1-20170620"
     build_policy = "missing"
     short_paths = True
     
@@ -24,31 +27,28 @@ class ConanFileInst(conans.ConanFile):
         if self.settings.os == "Macos" and self.settings.arch == "x86":
             raise Exception("Not supported x86 for OSx")
 
-    def get_filename(self):
-        major = str(self.options.version)[0]
-        (year, quarter) = self.year_quarter[str(self.options.version)]
-        if quarter == "q4":
-            release = "major"
-        else:
-            release = "update"
+    def get_path_filename(self):
+        (path, filename) = self.version_path_filename_map[str(self.options.version)]
         os_id = {"Macos": "mac", "Windows": "win32"}.get(str(self.settings.os))
-        return "gcc-arm-none-eabi-%s-%s-%s-%s-%s" % (major, year, quarter, release, os_id)
+        filename = filename % os_id
+        return path, filename
     
     def build(self):
-        major = str(self.options.version)[0]
-        (year, quarter) = self.year_quarter[str(self.options.version)]
+        (path, filename) = self.get_path_filename()
         ext = "tar.bz2" if not self.settings.os == "Windows" else "zip"
-        url = "https://developer.arm.com/-/media/Files/downloads/gnu-rm/%s-%s%s/%s.%s" % (major, year, quarter, self.get_filename(), ext)
+        url = "https://developer.arm.com/-/media/Files/downloads/gnu-rm/%s/%s.%s" % (path, filename, ext)
 
-        # https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-win32.exe
+        # https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-win32.zip
+        # https://developer.arm.com/-/media/Files/downloads/gnu-rm/6_1-2017q1/gcc-arm-none-eabi-6-2017-q1-update-%s-zip.zip
 
         dest_file = "file.%s" % ext
         self.output.info("Downloading: %s" % url)
         conans.tools.download(url, dest_file, verify=False)
-        conans.tools.unzip(dest_file, destination=self.get_filename())
+        conans.tools.unzip(dest_file, destination=filename)
 
     def package(self):
-        self.copy("*", dst="", src=self.get_filename())
+        (_, filename) = self.get_path_filename()
+        self.copy("*", dst="", src=filename)
 
     def package_info(self):
         if not self.package_folder is None:
